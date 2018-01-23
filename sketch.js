7e7279a803e20baccc9d06f51307d1482db3837f
Naia McNatt <nmcnatt@wlapcs.org>
2018-01-23 19:06:30 +0000

    a splash of color

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
-function setUp(){
+var redSlider, greenSlider, blueSlider;
+
+function setup(){
     createCanvas(600,400);
     background(0);
+    textSize(100);
+    text('RGB' rojoValue, verdeValue, azulValue,300,200);
+    
+    redSlider = createSlider(0,255,0);
+    redSlider.position(20,20);
+    
+    greenSlider = createSlider(0,255,0);
+    greenSlider.position(20,50);
+    
+    blueSlider = createSlider(0,255,0);
+    blueSlider.position(20,80);
 }
 
 function draw(){
+    rojoValue = redSlider.value();
+    verdeValue = greenSlider.value();
+    azulValue = blueSlider.value();
     
+    background(rojoValue, verdeValue, azulValue);
+    
+    if(rojoValue && verdeValue && azulValue < 100){
+        fill(255);
+    }else{
+        fill(0);
+    }
 }
